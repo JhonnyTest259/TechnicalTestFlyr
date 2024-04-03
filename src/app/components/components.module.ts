318b2194769afa089c1f0608a83a6b62bc17589f
Jhonatan_Mejia <jhobatrix@gmail.com>
2024-04-02 22:15:37 -0500

    se crea componente destinations, offers y cards

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,22 +3,20 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
-
-
+import { OffersComponent } from './offers/offers.component';
+import { CardsComponent } from './cards/cards.component';
+import { DestinationsComponent } from './destinations/destinations.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    BannerComponent
+    BannerComponent,
+    OffersComponent,
+    CardsComponent,
+    DestinationsComponent,
   ],
-  imports: [
-    CommonModule
-  ],
-  exports:[
-    HeaderComponent,
-    FooterComponent,
-    BannerComponent
-  ]
+  imports: [CommonModule],
+  exports: [HeaderComponent, FooterComponent, BannerComponent, OffersComponent],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
